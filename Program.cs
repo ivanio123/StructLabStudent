4b8d05216617bfa47b72c0c80419cf4604a690b2
whigga_02 <jodsonjod@gmail.com>
2020-06-04 12:31:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace struct_lab_student
+{
+    partial class Program
+    {
+        static Student[] ReadData(string fileName)
+        {
+            string[] inputFile = File.ReadAllLines(fileName);
+            Student[] studData = new Student[inputFile.Length];
+
+            for (int i = 0; i < inputFile.Length; i++)
+            {
+                studData[i] = new Student(inputFile[i]);
+            }
+            return studData;
+        }
+
+        static void runMenu(Student[] studs)
+        {
+            for (int i = 0; i < studs.Length; i++)
+            {
+                if ((studs[i].mathematicsMark == '2' || studs[i].mathematicsMark == '-' || studs[i].physicsMark == '2' || studs[i].physicsMark == '-' || studs[i].informaticsMark == '2' || studs[i].informaticsMark == '-')&&(studs[i].scholarship!=0))
+                {
+                    studs[i].scholarship = 0;
+                    Console.WriteLine("{0} {1} {2} {3}", studs[i].surName, studs[i].mathematicsMark, studs[i].physicsMark, studs[i].informaticsMark);
+                }
+                using (StreamWriter SW = File.AppendText("data_new.txt"))
+                {
+                    SW.WriteLine("{0}  {1}  {2}  {3}  {4}  {5}  {6}  {7}  {8}", studs[i].surName, studs[i].firstName, studs[i].patronymic, studs[i].sex, studs[i].dateOfBirth, studs[i].mathematicsMark, studs[i].physicsMark, studs[i].informaticsMark, studs[i].scholarship);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string path = @"input.txt";
+            Student[] studs = ReadData(path);
+            runMenu(studs);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace struct_lab_student
+{
+    struct Student
+    {
+        public string surName;
+        public string firstName;
+        public string patronymic;
+        public char sex;
+        public string dateOfBirth;
+        public char mathematicsMark;
+        public char physicsMark;
+        public char informaticsMark;
+        public int scholarship;
+
+        public Student(string lineWithAllData)
+        {
+            string[] dataOfOneStud = lineWithAllData.Split(' ');
+
+            surName = dataOfOneStud[0];
+            firstName = dataOfOneStud[1];
+            patronymic = dataOfOneStud[2];
+            sex = Convert.ToChar(dataOfOneStud[3]);
+            dateOfBirth = dataOfOneStud[4];
+            mathematicsMark = Convert.ToChar(dataOfOneStud[5]);
+            physicsMark = Convert.ToChar(dataOfOneStud[6]);
+            informaticsMark = Convert.ToChar(dataOfOneStud[7]);
+            scholarship = Convert.ToInt32(dataOfOneStud[8]);
+        }
+    }
+}
